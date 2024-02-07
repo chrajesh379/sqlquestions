61274f052fba7a24d2bd4ca5fb9ba6693324aadb
chrajesh379 <155586966+chrajesh379@users.noreply.github.com>
2024-02-07 10:10:05 -0800

    Update snapshot_backfill_dates.sql

--- BiqQuery/snapshot_backfill_dates.sql
+++ BiqQuery/snapshot_backfill_dates.sql
@@ -21,10 +21,11 @@ with cte as
   SELECT '2024-01-18' AS snapshot_date
 ),
 cte_1 AS (
-SELECT date(snapshot_date) as snapshot_date ,COALESCE(lag(date(snapshot_date),1)OVER(order by snapshot_date),date(snapshot_date)) prev_snapshot_date FROM cte
+SELECT date(snapshot_date) as snapshot_date ,COALESCE(LEAD(date(snapshot_date),1)OVER(order by snapshot_date),date(CURRENT_DATE())) prev_snapshot_date FROM cte
 )
 
 --select * from cte_1
-select fp.date,cte_1.snapshot_date, cte_1.prev_snapshot_date from `ite-aia-datalake.it_dm.tb_pipe_fiscal_periods` fp
-inner join cte_1 on fp.date>= cte_1.prev_snapshot_date and fp.date<cte_1.snapshot_date
+select fp.date,cte_1.snapshot_date--, cte_1.prev_snapshot_date 
+from `ite-aia-datalake.it_dm.tb_pipe_fiscal_periods` fp
+inner join cte_1 on fp.date>= cte_1.snapshot_date and fp.date<cte_1.prev_snapshot_date
 order by 1
