fff005d0d235f566a47f40e810e22cfd8223ebfa
chrajesh379 <155586966+chrajesh379@users.noreply.github.com>
2024-02-07 10:13:53 -0800

    Create snapshot_backfill.sql

---
+++ BiqQuery/snapshot_backfill.sql
@@ -0,0 +1,31 @@
+with cte as 
+(
+  SELECT '2024-01-01' AS snapshot_date
+  UNION ALL
+  SELECT '2024-01-02' AS snapshot_date
+    UNION ALL
+  SELECT '2024-01-03' AS snapshot_date
+      UNION ALL
+  SELECT '2024-01-04' AS snapshot_date
+      UNION ALL
+  SELECT '2024-01-10' AS snapshot_date
+       UNION ALL
+  SELECT '2024-01-11' AS snapshot_date
+         UNION ALL
+  SELECT '2024-01-12' AS snapshot_date
+           UNION ALL
+  SELECT '2024-01-14' AS snapshot_date
+             UNION ALL
+  SELECT '2024-01-17' AS snapshot_date
+               UNION ALL
+  SELECT '2024-01-18' AS snapshot_date
+),
+cte_1 AS (
+SELECT date(snapshot_date) as snapshot_date ,COALESCE(LEAD(date(snapshot_date),1)OVER(order by snapshot_date),date(CURRENT_DATE())) prev_snapshot_date FROM cte
+)
+
+--select * from cte_1
+select fp.date,cte_1.snapshot_date--, cte_1.prev_snapshot_date 
+from (SELECT * FROM UNNEST(GENERATE_DATE_ARRAY('2023-10-01', '2024-12-31', INTERVAL 1 DAY)) AS date) fp
+inner join cte_1 on fp.date>= cte_1.snapshot_date and fp.date<cte_1.prev_snapshot_date
+order by 1
