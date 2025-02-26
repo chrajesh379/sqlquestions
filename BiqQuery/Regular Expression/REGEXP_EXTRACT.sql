e61320ea6bb9ae9822ed4a7a310acc7ed68fe000
chrajesh379 <155586966+chrajesh379@users.noreply.github.com>
2025-02-25 17:12:36 -0800

    Create REGEXP_EXTRACT.sql

---
+++ BiqQuery/Regular Expression/REGEXP_EXTRACT.sql
@@ -0,0 +1,8 @@
+SELECT province_state,country_region,combined_key,
+REGEXP_EXTRACT(combined_key,r"^[^,]*") AS city -- starting with any number of characters except , 
+-- It will pull entire substring till it find , 
+-- * means 0 or more number of characters
+FROM `bigquery-public-data.covid19_jhu_csse.summary`
+WHERE date='2020-04-04'
+
+;
