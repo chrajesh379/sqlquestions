64180a692a8c5e192ecbd9e851cd260af2e395b5
chrajesh379 <155586966+chrajesh379@users.noreply.github.com>
2025-02-27 15:27:22 -0800

    Update REGEXP_EXTRACT.sql

--- BiqQuery/Regular Expression/REGEXP_EXTRACT.sql
+++ BiqQuery/Regular Expression/REGEXP_EXTRACT.sql
@@ -7,3 +7,25 @@ FROM `bigquery-public-data.covid19_jhu_csse.summary`
 WHERE date='2020-04-04'
 
 ;
+
+
+with cte as (
+  SELECT """2024-10-13 12:02:01,938 STUDENT-INFO  :{'enabled': '123456789'}#abcdfed updatee: student_number: 132546$ Class: 2$ Address: $ Maths: 70$ English: 60$ Chemistry: 45$ Physics: 80$ city: Bangalore$ 
+""" AS col_value
+)
+SELECT col_value
+-- starting from student_number string with any number of characters  
+,REGEXP_EXTRACT(col_value,r"student_number:\s.*") student_number_after 
+-- starting from student_number string with any number of characters except student_number: text 
+-- when ever we have used () it will behave like capturing group (submatch)
+,REGEXP_EXTRACT(col_value,r"student_number:\s(.*)") student_number_after_without_text
+-- starting from student_number string with any number of characters except student_number: text 
+-- when ever we have used () it will behave like capturing group (submatch)
+-- in the submatch doesn't start with $
+,REGEXP_EXTRACT(col_value,r"student_number:\s([^\$]*)") student_number
+,REGEXP_EXTRACT(col_value,r"Class:\s([^\$]*)") Class
+,REGEXP_EXTRACT(col_value,r"Address:\s([^\$]*)") Address
+FROM cte
+
+;
+
